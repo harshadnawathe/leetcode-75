86531dcff7033bdcd08db933bc68f59fae8178a5
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-06 19:19:06 +0530

    refactor(product_of_array_except_self): use single loop

--- solutions/product_of_array_except_self/solution.py
+++ solutions/product_of_array_except_self/solution.py
@@ -1,15 +1,16 @@
 from typing import List
-from operator import mul
-from itertools import accumulate
 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left_product = list(accumulate(nums, mul))
-        right_product = list(accumulate(nums[::-1], mul))[::-1]
+        n = len(nums)
+        result = [1] * n
+        pre, post = 1, 1
 
-        return [
-            (left_product[i - 1] if i > 0 else 1)
-            * (right_product[i + 1] if i < len(nums) - 1 else 1)
-            for i in range(len(nums))
-        ]
+        for i in range(n):
+            result[i] *= pre
+            pre *= nums[i]
+            result[n - i - 1] *= post
+            post *= nums[n - i - 1]
+
+        return result
