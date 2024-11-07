9aaab6858be117507c0191be9ba79492c9e6857b
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-07 08:30:09 +0530

    refactor(move_zeroes): simplify solution
    Use a single loop instead of multiple inner loops

--- solutions/move_zeroes/solution.py
+++ solutions/move_zeroes/solution.py
@@ -3,14 +3,8 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        i, j = 0, 0
-        while j < len(nums):
-            # Skip all zero elements
-            while j < len(nums) and not nums[j]:
-                j += 1
-
-            # Swap all non-zero elements
-            while j < len(nums) and nums[j]:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j]:
                 nums[i], nums[j] = nums[j], nums[i]
                 i += 1
-                j += 1
