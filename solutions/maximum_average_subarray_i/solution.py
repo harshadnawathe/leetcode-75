9de65442bb83dbfaf1ac5a718b098df8a380965c
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-07 14:47:10 +0530

    refactor(maximum_average_subarray_i): simplify solution
    Instead of adjusting the sum for each shift in the window,
    just calculating the maximum difference that will be added
    to the sum of first k elements.

--- solutions/maximum_average_subarray_i/solution.py
+++ solutions/maximum_average_subarray_i/solution.py
@@ -1,22 +1,13 @@
 from typing import List
-from math import inf
 
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        max_avg = -inf
-        begin = 0
-        sum = 0
+        max_diff = diff = 0
 
-        for curr, num in enumerate(nums):
-            win_size = curr - begin + 1
-            sum += num
+        for i in range(len(nums) - k):
+            diff += nums[i + k] - nums[i]
+            if max_diff < diff:
+                max_diff = diff
 
-            if win_size == k:
-                max_avg = max(max_avg, sum / k)
-
-                # Shift window
-                sum -= nums[begin]
-                begin += 1
-
-        return max_avg
+        return (sum(nums[:k]) + max_diff) / k
