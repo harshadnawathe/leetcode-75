4a1b0214b7841eefda06633025c2defd6ca1ad40
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-16 07:15:54 +0530

    refactor(commons.list): use dataclass to implement ListNode

--- commons/list.py
+++ commons/list.py
@@ -1,37 +1,11 @@
-from typing import Iterable, Iterator, Optional
+from dataclasses import dataclass
+from typing import Iterable, Optional
 
 
+@dataclass
 class ListNode:
-    def __init__(self, val=0, next: Optional["ListNode"] = None) -> None:
-        self.val = val
-        self.next = next
-
-    def __eq__(self, other) -> bool:
-        if self is other:
-            return True
-
-        if not isinstance(other, ListNode):
-            return False
-
-        node, node_other = self, other
-
-        while node and node_other:
-            if node.val != node_other.val:
-                return False
-
-            node = node.next
-            node_other = node_other.next
-
-        return node is None and node_other is None
-
-    def __iter__(self) -> Iterator:
-        node = self
-        while node:
-            yield node.val
-            node = node.next
-
-    def __str__(self) -> str:
-        return f"[{', '.join(map(str, self))}]"
+    val: int = 0
+    next: Optional["ListNode"] = None
 
     @classmethod
     def from_values(cls, values: Iterable) -> Optional["ListNode"]:
