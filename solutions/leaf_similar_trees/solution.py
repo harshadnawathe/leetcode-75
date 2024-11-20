f989a3a037604bbc4431da04aa42f2095af96903
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-20 17:20:18 +0530

    refactor(leaf_similar_trees): use pythonic style

--- solutions/leaf_similar_trees/solution.py
+++ solutions/leaf_similar_trees/solution.py
@@ -1,7 +1,8 @@
-from typing import Optional
-from commons.tree import TreeNode
-from itertools import zip_longest, starmap
+from itertools import starmap, zip_longest
 from operator import eq
+from typing import Optional
+
+from commons.tree import TreeNode
 
 
 class Solution:
@@ -9,16 +10,14 @@ class Solution:
         return all(starmap(eq, zip_longest(self.leaves(root1), self.leaves(root2))))
 
     def leaves(self, root: Optional[TreeNode]):
-        nodes = [root] if root is not None else []
+        if not root:
+            return
 
+        nodes = [root]
         while nodes:
             node = nodes.pop()
 
-            if node.left is not None:
-                nodes.append(node.left)
+            nodes.extend(child for child in (node.left, node.right) if child)
 
-            if node.right is not None:
-                nodes.append(node.right)
-
-            if node.left is None and node.right is None:
+            if not (node.left or node.right):
                 yield node.val
