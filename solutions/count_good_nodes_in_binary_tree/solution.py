f91f41fdfeabe831d4ed5d9037d3a15fac889fd3
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-20 17:04:50 +0530

    refactor(count_good_nodes_in_binary_tree): use pythonic style

--- solutions/count_good_nodes_in_binary_tree/solution.py
+++ solutions/count_good_nodes_in_binary_tree/solution.py
@@ -1,24 +1,21 @@
 from commons.tree import TreeNode
-from math import inf
 
 
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         good_node_count = 0
 
-        nodes = [(root, -inf)] if root is not None else []
+        stack = [(root, root.val)]
 
-        while nodes:
-            node, max_so_far = nodes.pop()
+        while stack:
+            node, max_so_far = stack.pop()
 
             if max_so_far <= node.val:
-                max_so_far = node.val
                 good_node_count += 1
+                max_so_far = node.val
 
-            if node.left is not None:
-                nodes.append((node.left, max_so_far))
-
-            if node.right is not None:
-                nodes.append((node.right, max_so_far))
+            stack.extend(
+                (child, max_so_far) for child in (node.left, node.right) if child
+            )
 
         return good_node_count
