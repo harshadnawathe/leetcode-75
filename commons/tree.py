b8770eec8ecc1a40b501d96e22801d4c5d0f3e4c
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-20 12:30:21 +0530

    feat(commons.tree): find node with given value

--- commons/tree.py
+++ commons/tree.py
@@ -12,6 +12,23 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def get_node_with_val(self, val: int) -> Optional["TreeNode"]:
+        nodes: list[TreeNode] = [self]
+
+        while nodes:
+            node = nodes.pop(0)
+
+            if node.val == val:
+                return node
+
+            if node.left is not None:
+                nodes.append(node.left)
+
+            if node.right is not None:
+                nodes.append(node.right)
+
+        return None
+
     @classmethod
     def from_vals(cls, vals: Iterable[Optional[int]]) -> Optional["TreeNode"]:
         vals_iter = iter(vals)
