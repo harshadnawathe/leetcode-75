3cd0da03a0efaaa851a02c57707228b600c21885
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-20 17:25:22 +0530

    refactor(path_sum_iii): remove instance methods and variables

--- solutions/path_sum_iii/solution.py
+++ solutions/path_sum_iii/solution.py
@@ -1,29 +1,23 @@
-from commons.tree import TreeNode
-from typing import DefaultDict, Optional
 from collections import defaultdict
+from typing import Optional
+
+from commons.tree import TreeNode
 
 
 class Solution:
-    def path_sum(self, node: Optional[TreeNode], curr_sum: int) -> int:
-        if node is None:
-            return 0
-
-        curr_sum += node.val
-
-        result = self.count[curr_sum - self.target]
-
-        self.count[curr_sum] += 1
-
-        result += self.path_sum(node.left, curr_sum) + self.path_sum(
-            node.right, curr_sum
-        )
-
-        self.count[curr_sum] -= 1
-
-        return result
-
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        self.count: DefaultDict[int, int] = defaultdict(int, [(0, 1)])
-        self.target = targetSum
+        def dfs(node: Optional[TreeNode], curr_sum: int) -> int:
+            if not node:
+                return 0
 
-        return self.path_sum(root, 0)
+            curr_sum += node.val
+            count = prefix_sums[curr_sum - targetSum]
+
+            prefix_sums[curr_sum] += 1
+            count += dfs(node.left, curr_sum) + dfs(node.right, curr_sum)
+            prefix_sums[curr_sum] -= 1  # Backtrack
+
+            return count
+
+        prefix_sums = defaultdict(int, {0: 1})
+        return dfs(root, 0)
