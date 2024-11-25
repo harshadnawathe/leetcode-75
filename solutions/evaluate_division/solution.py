dba616a4907d907227ab31e753bfaf853c4f9235
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-25 19:16:41 +0530

    refactor(evaluate_division): simplify solution

--- solutions/evaluate_division/solution.py
+++ solutions/evaluate_division/solution.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Dict, List, Set, Tuple
+from typing import List, Dict, Tuple
 
 
 class Solution:
@@ -8,43 +8,33 @@ class Solution:
     ) -> List[float]:
         graph = self._build_graph(equations, values)
 
-        visited: Set[str] = set()
+        def dfs(curr: str, target: str, visited: set, product: float) -> float:
+            if curr == target:
+                return product
+            visited.add(curr)
 
-        def dfs(curr: str, end: str, product_so_far: float = 1.0) -> float:
-            if curr == end:
-                return product_so_far
-
-            try:
-                visited.add(curr)
-
-                for neighbour, value in graph[curr]:
-                    if neighbour in visited:
-                        continue
-                    ans = dfs(neighbour, end, product_so_far * value)
-                    if ans != -1:
-                        return ans
-            finally:
-                visited.remove(curr)
+            for neighbor, value in graph[curr]:
+                if neighbor not in visited:
+                    result = dfs(neighbor, target, visited, product * value)
+                    if result != -1:
+                        return result
 
             return -1
 
-        answers: List[float] = []
-
-        for [A, B] in queries:
-            if (A not in graph) or (B not in graph):
-                answers.append(-1)
+        results = []
+        for A, B in queries:
+            if A not in graph or B not in graph:
+                results.append(-1.0)
             else:
-                answers.append(dfs(A, B))
+                results.append(dfs(A, B, set(), 1.0))
 
-        return answers
+        return results
 
     def _build_graph(
         self, equations: List[List[str]], values: List[float]
     ) -> Dict[str, List[Tuple[str, float]]]:
-        graph: Dict[str, List[Tuple[str, float]]] = defaultdict(list)
-
-        for [A, B], value in zip(equations, values):
+        graph = defaultdict(list)
+        for (A, B), value in zip(equations, values):
             graph[A].append((B, value))
             graph[B].append((A, 1 / value))
-
         return graph
