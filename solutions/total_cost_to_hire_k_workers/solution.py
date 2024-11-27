7b90abe591d1af7aaedd737cee9700cd3889ec5f
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-27 21:37:57 +0530

    refactor(total_cost_to_hire_k_workers): simplify solution
    - Use slicing syntax to create heaps
    - Use single loop for computing total cost

--- solutions/total_cost_to_hire_k_workers/solution.py
+++ solutions/total_cost_to_hire_k_workers/solution.py
@@ -4,56 +4,26 @@ from heapq import heapify, heappush, heappop
 
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
-        left, right = 0, len(costs)
-        left_heap, right_heap = [], []
+        left = min(candidates, len(costs))
+        right = max(candidates, len(costs) - candidates)
 
-        for _ in range(candidates):
-            if left < right:
-                left_heap.append(costs[left])
-                left += 1
-            else:
-                break
-
+        left_heap = costs[:left]
         heapify(left_heap)
 
-        for _ in range(candidates):
-            if left < right:
-                right -= 1
-                right_heap.append(costs[right])
-            else:
-                break
-
+        right_heap = costs[right:]
         heapify(right_heap)
 
         total_cost = 0
-        rounds = 0
-
-        while rounds < k and right_heap and left_heap:
-            rounds += 1
-
-            if right_heap[0] < left_heap[0]:
-                total_cost += heappop(right_heap)
-                if left < right:
-                    right -= 1
-                    heappush(right_heap, costs[right])
-            else:
+        for _ in range(k):
+            if not right_heap or (left_heap and left_heap[0] <= right_heap[0]):
                 total_cost += heappop(left_heap)
                 if left < right:
                     heappush(left_heap, costs[left])
                     left += 1
-
-        while right_heap and rounds < k:
-            rounds += 1
-            total_cost += heappop(right_heap)
-            if left < right:
-                right -= 1
-                heappush(right_heap, costs[right])
-
-        while left_heap and rounds < k:
-            rounds += 1
-            total_cost += heappop(left_heap)
-            if left < right:
-                heappush(left_heap, costs[left])
-                left += 1
+            else:
+                total_cost += heappop(right_heap)
+                if left < right:
+                    right -= 1
+                    heappush(right_heap, costs[right])
 
         return total_cost
