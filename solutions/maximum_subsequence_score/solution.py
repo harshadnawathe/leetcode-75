bef2ce3752fc882bb7600de3fb6e32b9a92691fd
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-27 15:00:44 +0530

    refactor(maximum_subsequence_score): simplify solution
    - remove Pair and use basic Tuple
    - use list comprehension instead accumulate and other itertools

--- solutions/maximum_subsequence_score/solution.py
+++ solutions/maximum_subsequence_score/solution.py
@@ -1,35 +1,22 @@
-import heapq
-from collections import namedtuple
-from itertools import starmap
+from heapq import heapify, heappop, heappush
 from typing import List
 
 
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        Pair = namedtuple("Pair", ["first", "second"])
-        pairs = sorted(starmap(Pair, zip(nums1, nums2)), key=lambda p: -p.second)
+        pairs = sorted(zip(nums1, nums2), key=lambda p: -p[1])
 
-        print(pairs)
+        min_heap = [num1 for num1, _ in pairs[:k]]
+        heapify(min_heap)
 
-        min_heap = list(map(lambda p: p.first, pairs[:k]))
-        heapq.heapify(min_heap)
+        top_k_sum = sum(min_heap)
 
-        print(min_heap)
+        max_score = top_k_sum * pairs[k - 1][1]
 
-        top_k_sum = sum(map(lambda p: p.first, pairs[:k]))
+        for num1, num2 in pairs[k:]:
+            top_k_sum += num1 - heappop(min_heap)
+            heappush(min_heap, num1)
 
-        print(top_k_sum)
-
-        max_score = top_k_sum * pairs[k - 1].second
-
-        for i in range(k, len(pairs)):
-            top_k_sum += pairs[i].first - heapq.heappop(min_heap)
-
-            heapq.heappush(min_heap, pairs[i].first)
-
-            score = top_k_sum * pairs[i].second
-
-            if max_score < score:
-                max_score = score
+            max_score = max(max_score, top_k_sum * num2)
 
         return max_score
