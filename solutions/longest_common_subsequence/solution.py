b939de8f7eb5acfbe7ff23a5fc3255e4035d7394
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-12-05 09:16:12 +0530

    refactor(longest_common_subsequence): use Pythonic style
    1. Use reversed range
    2. Use if-else expression

--- solutions/longest_common_subsequence/solution.py
+++ solutions/longest_common_subsequence/solution.py
@@ -1,16 +1,14 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        ni, nj = len(text1), len(text2)
-        dp = [[0] * (nj + 1) for _ in range(ni + 1)]
+        m, n = len(text1), len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-        for i in range(ni - 1, -1, -1):
-            for j in range(nj - 1, -1, -1):
-                if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i + 1][j + 1]
-                else:
-                    dp[i][j] = max(
-                        dp[i][j + 1],
-                        dp[i + 1][j],
-                    )
+        for i in reversed(range(m)):
+            for j in reversed(range(n)):
+                dp[i][j] = (
+                    1 + dp[i + 1][j + 1]
+                    if text1[i] == text2[j]
+                    else max(dp[i + 1][j], dp[i][j + 1])
+                )
 
         return dp[0][0]
