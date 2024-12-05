ee63ff190715bfe5b9d14c8f908978f79381daed
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-12-05 08:41:38 +0530

    perf(unique_paths): optimize memory

--- solutions/unique_paths/solution.py
+++ solutions/unique_paths/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[1] * n for _ in range(m)]
+        dp = [1] * n
 
-        for r in range(1, m):
+        for _ in range(1, m):
             for c in range(1, n):
-                dp[r][c] = dp[r][c - 1] + dp[r - 1][c]
+                dp[c] = dp[c - 1] + dp[c]
 
-        return dp[-1][-1]
+        return dp[-1]
