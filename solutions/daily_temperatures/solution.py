e4a49ebe04b22dc0653d4aed72839bf6c7173e22
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-12-06 14:01:23 +0530

    refactor(daily_temperatures): simplify solution

--- solutions/daily_temperatures/solution.py
+++ solutions/daily_temperatures/solution.py
@@ -4,10 +4,10 @@ from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         result = [0] * len(temperatures)
-        stack = [0]
+        stack = []
 
-        for today in range(1, len(temperatures)):
-            while len(stack) and temperatures[stack[-1]] < temperatures[today]:
+        for today, temperature in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temperature:
                 colder_day = stack.pop()
                 result[colder_day] = today - colder_day
             stack.append(today)
