edc7353feab35070dc6f868db1a11de3efeeccfd
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-12-06 12:28:36 +0530

    refactor(non_overlapping_intervals): simplify solution

--- solutions/non_overlapping_intervals/solution.py
+++ solutions/non_overlapping_intervals/solution.py
@@ -3,15 +3,16 @@ from typing import List
 
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        n = len(intervals)
         intervals.sort(key=lambda x: x[1])
 
-        prev = 0
-        count = 1
+        count, prev_end = 0, float("-inf")
 
-        for i in range(1, n):
-            if intervals[i][0] >= intervals[prev][1]:
-                prev = i
+        for start, end in intervals:
+            if start >= prev_end:
+                # No overlap
+                prev_end = end
+            else:
+                # Has overlap
                 count += 1
 
-        return n - count
+        return count
