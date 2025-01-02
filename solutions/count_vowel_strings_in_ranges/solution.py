0dc7a8b6a10c2f3ba2276222cd49145076e079d9
Harshad Nawathe <harshad.nawathe@gmail.com>
2025-01-02 11:48:29 +0530

    refactor(count_vowel_strings_in_ranges): simplify solution
    - Construct set using literal definition.
    - Simplify query computation

--- solutions/count_vowel_strings_in_ranges/solution.py
+++ solutions/count_vowel_strings_in_ranges/solution.py
@@ -5,11 +5,8 @@ from operator import add
 
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        vowels = set(["a", "e", "i", "o", "u"])
+        vowels = {"a", "e", "i", "o", "u"}
         is_vowel_str = [s[0] in vowels and s[-1] in vowels for s in words]
-        vowel_str_count = list(accumulate(is_vowel_str, add))
+        vowel_str_count = [0] + list(accumulate(is_vowel_str, add))
 
-        return [
-            vowel_str_count[e] - vowel_str_count[b] + is_vowel_str[b]
-            for b, e in queries
-        ]
+        return [vowel_str_count[e + 1] - vowel_str_count[b] for b, e in queries]
